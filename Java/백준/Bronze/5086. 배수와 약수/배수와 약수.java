9d302eb5fdad39d4aa50e4b1ff9ccb4205f87f34
서만기 <seo_m98@naver.com>
2024-08-12 08:29:13 +0900

    [Bronze III] Title: 배수와 약수, Time: 88 ms, Memory: 12812 KB -BaekjoonHub

--- Java/백준/Bronze/5086. 배수와 약수/배수와 약수.java
+++ Java/백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -1,14 +1,15 @@
-import java.util.Scanner;
-public class Main {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		while(true) {
-			int first = in.nextInt();
-			int second = in.nextInt();
-			if(first == 0 && second == 0)break;
-			if(second % first == 0)System.out.println("factor");
-			else if(first % second == 0)System.out.println("multiple");
-			else System.out.println("neither");
-		}
-	}
+import java.util.*;
+interface Main{
+    static void main(String[]x){
+        Scanner sc=new Scanner(System.in);
+        String ans;
+        while(true) {
+            int i=sc.nextInt(),j=sc.nextInt();
+            if(i==0&&j==0)break;
+            if(j%i==0)ans="factor";
+            else if(i%j==0)ans="multiple";
+            else ans="neither";
+            System.out.println(ans);
+        }
+    }
 }
