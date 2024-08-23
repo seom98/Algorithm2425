d28fedf977107f4eb311b176efbf1e9af3cf22ff
seom98 <seo_m98@naver.com>
2024-08-23 17:21:07 +0900

    [Gold V] Title: 평범한 배낭, Time: 136 ms, Memory: 13520 KB -BaekjoonHub

--- Java/백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ Java/백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,9 +1,1 @@
-import java.util.*;
-interface Main{
-	static void main(String[]z){
-		Scanner c=new Scanner(System.in);
-		int N=c.nextInt(),M=c.nextInt(),d[]=new int[M+1];
-		while(N-->0)for(int i=M,W=c.nextInt(),C=c.nextInt();i>=W;i--)d[i]=Math.max(d[i],d[i-W]+C);
-		System.out.print(d[M]);
-	}
-}
+import java.util.*;interface Main{static void main(String[]z){Scanner c=new Scanner(System.in);int N=c.nextInt(),M=c.nextInt(),d[]=new int[M+1];while(N-->0)for(int i=M,W=c.nextInt(),C=c.nextInt();i>=W;i--)d[i]=Math.max(d[i],d[i-W]+C);System.out.print(d[M]);}}
