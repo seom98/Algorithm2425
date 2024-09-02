41f1280859ad5f2e0163003c618ecb41ba3007f4
seom98 <seo_m98@naver.com>
2024-09-02 20:44:13 +0900

    [Bronze I] Title: 0의 개수, Time: 640 ms, Memory: 293428 KB -BaekjoonHub

--- Java/백준/Bronze/11170. 0의 개수/0의 개수.java
+++ Java/백준/Bronze/11170. 0의 개수/0의 개수.java
@@ -1,17 +1,1 @@
-import java.util.*;
-interface Main{
-	public static void main(String[]z){
-		Scanner s=new Scanner(System.in);
-		int T=s.nextInt(),N,M,C;
-		while(T-->0){
-			N=s.nextInt();M=s.nextInt();C=0;
-			for(int i=N;i<=M;i++){
-				String a=Integer.toString(i);
-				for(int j=0;j<a.length();j++){
-					if(a.charAt(j)=='0')C++;
-				}
-			}
-			System.out.println(C);
-		}
-	}
-}
+import java.util.*;interface Main{static void main(String[]z){Scanner s=new Scanner(System.in);int T=s.nextInt(),N,M,C;while(T-->0){N=s.nextInt();M=s.nextInt();C=0;for(int i=N;i<=M;i++){String a=Integer.toString(i);for(int j=0;j<a.length();j++)if(a.charAt(j)=='0')C++;}System.out.println(C);}}}
