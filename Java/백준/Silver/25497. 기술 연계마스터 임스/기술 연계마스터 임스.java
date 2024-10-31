ba0b3c28e47e09374d074ee2d4c269af9eb641fd
seom98 <seo_m98@naver.com>
2024-10-31 22:10:49 +0900

    [Silver V] Title: 기술 연계마스터 임스, Time: 312 ms, Memory: 22756 KB -BaekjoonHub

--- Java/백준/Silver/25497. 기술 연계마스터 임스/기술 연계마스터 임스.java
+++ Java/백준/Silver/25497. 기술 연계마스터 임스/기술 연계마스터 임스.java
@@ -2,17 +2,13 @@ import java.util.*;
 interface Main{
 	static void main(String[]z){
 		Scanner s=new Scanner(System.in);
-		int N=s.nextInt(),n=0,a=0,b=0,m=0;
-		char[]c=s.next().toCharArray();
-		l:while(N-->0){
-			switch(c[n++]){
-				case 'L':a++;break;
-				case 'R':if(--a<0)break l;else m++;break;
-				case 'S':b++;break;
-				case 'K':if(--b<0)break l;else m++;break;
-				default:m++;
-			}
-		}
+		s.next();int a=0,b=0,m=0;var c=s.next();
+		l:for(var x:c.toCharArray())
+		    if(x=='L')a++;
+			else if(x=='R'){if(--a<0)break l;m++;}
+			else if(x=='S')b++;
+			else if(x=='K'){if(--b<0)break l;m++;}
+			else m++;
 		System.out.print(m);
 	}
 }
