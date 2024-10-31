d949c686c61c583d46a1465b88624494864087f1
seom98 <seo_m98@naver.com>
2024-10-31 23:50:06 +0900

    [Silver IV] Title: 틱택토 이기기, Time: 124 ms, Memory: 14468 KB -BaekjoonHub

--- Java/백준/Silver/9290. 틱택토 이기기/틱택토 이기기.java
+++ Java/백준/Silver/9290. 틱택토 이기기/틱택토 이기기.java
@@ -1,29 +1,29 @@
 import java.util.*;
 interface Main{
-	static void main(String[]y){
+	static void main(String[]z){
 		Scanner s=new Scanner(System.in);
 		int N=s.nextInt(),n=1;
 		StringBuilder sb=new StringBuilder();
 		while(N-->0){
 			sb.append("Case "+n+++":\n");
-			int m=-1,r[][]=new int[2][2],z=0,o,p,q,u;
+			int M=-1,t[][]=new int[2][2],b=0,i=-1,j=0;
 			char c[][]=new char[3][3];
-			while(++m<3)c[m]=s.next().toCharArray();
+			while(++M<3)c[M]=s.next().toCharArray();
 			char a=s.next().charAt(0);
-			for(int i=0;i<3;i++){
-				for(int j=0;j<3;j++){
-					if(c[i][j]==a){
-						r[z][0]=i;
-						r[z++][1]=j;
-					}
+			while(++i<3){
+				j=-1;
+				while(++j<3){
+					if(c[i][j]==a){t[b][0]=i;t[b++][1]=j;}
 				}
 			}
-			o=r[0][0];p=r[0][1];q=r[1][0];u=r[1][1];
-			if(o==q)c[o][3-p-u]=a;
-			else if(p==u)c[3-o-q][p]=a;
-			else c[3-o-q][3-p-u]=a;
-			for(int i=0;i<3;i++){
-				for(int j=0;j<3;j++)sb.append(c[i][j]);
+			int o=t[0][0],p=t[0][1],q=t[1][0],r=t[1][1];
+			if(o==q)c[o][3-p-r]=a;
+			else if(p==r)c[3-o-q][p]=a;
+			else c[3-o-q][3-p-r]=a;
+			i=-1;
+			while(++i<3){
+				j=-1;
+				while(++j<3)sb.append(c[i][j]);
 				sb.append('\n');
 			}
 		}
