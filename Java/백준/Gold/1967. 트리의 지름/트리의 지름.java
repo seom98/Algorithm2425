152431a5fcd2b28889ef6abe40ab7c98e19093e7
seom98 <seo_m98@naver.com>
2024-11-13 19:30:13 +0900

    [Gold IV] Title: 트리의 지름, Time: 92 ms, Memory: 13348 KB -BaekjoonHub

--- Java/백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ Java/백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -1,17 +1,14 @@
 import java.util.*;
-import java.io.*;
 class Main{
 	static int N,MAX=0,a[],b[],v[];
 	static ArrayList<ArrayList<int[]>>list=new ArrayList<>();
 
-	public static void main(String[]z)throws IOException{
-		BufferedReader S=new BufferedReader(new InputStreamReader(System.in));
-		N=Integer.parseInt(S.readLine())+1;a=new int[N];b=new int[N];v=new int[N];
+	public static void main(String[]z)throws Exception{
+		R s=new R();
+		N=s.N()+1;a=new int[N];b=new int[N];v=new int[N];
 		for(int n=0;n<N;n++)list.add(new ArrayList<>());
-		StringTokenizer T;
 		for(int n=2;n<N;n++){
-			T=new StringTokenizer(S.readLine());
-			int i=Integer.parseInt(T.nextToken()),j=Integer.parseInt(T.nextToken()),k=Integer.parseInt(T.nextToken());
+			int i=s.N(),j=s.N(),k=s.N();
 			list.get(i).add(new int[]{j,k});
 		}
 		for(int n=N-1;n>0;n--){
@@ -49,4 +46,14 @@ class Main{
 			}
 		}
 	}
+}
+class R{
+	private final int S=1<<13;
+	private byte B[] = new byte[S],c;
+	private int I,s,n;
+	public int N()throws Exception{n=0;while((c=r())<=32);do n=(n<<3)+(n<<1)+(c&15);while(N(c=r()));return n;}
+
+	private boolean N(byte c){return 47<c&&c<58;}
+
+	private byte r()throws Exception{if(I==s){s=System.in.read(B,I=0,S);if(s<0)B[0]=-1;}return B[I++];}
 }
