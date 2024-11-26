235d7116ae51a226e4f80e7eb4ebb8307c2f8407
seom98 <seo_m98@naver.com>
2024-11-27 01:14:27 +0900

    [Gold III] Title: 별자리 만들기, Time: 264 ms, Memory: 20100 KB -BaekjoonHub

--- Java/백준/Gold/4386. 별자리 만들기/별자리 만들기.java
+++ Java/백준/Gold/4386. 별자리 만들기/별자리 만들기.java
@@ -1,57 +1,20 @@
 import java.util.*;
-class Node{
-	int x;
-	int y;
-	double d;
-	Node(int x,int y,double d){
-		this.x=x;
-		this.y=y;
-		this.d=d;
-	}
-}
-class Main {
+class Node{int x;int y;double d;Node(int x,int y,double d){this.x=x;this.y=y;this.d=d;}}
+class Main{
 	static int[] arr;
 	public static void main(String[]z){
 		Scanner s=new Scanner(System.in);
-		int N=s.nextInt();
-		Double[][] point=new Double[N+1][2];
-		arr=new int[N+1];
-		for(int i=1;i<N+1;i++){
-			point[i][0]=s.nextDouble();
-			point[i][1]=s.nextDouble();
-			arr[i]=i;
-		}
-		ArrayList<Node>list=new ArrayList<>();
-		for(int i=1;i<N;i++){
-			for(int j=i+1;j<N+1;j++){
-				//i가 첫번째점, j가 두번째 점, 둘사이 거리 로 저장
-				list.add(new Node(i,j,Math.sqrt(Math.pow(point[i][0]-point[j][0],2)+Math.pow(point[i][1]-point[j][1],2))));
-			}
-		}
-
+		int N=s.nextInt(),pick=0,px,py;arr=new int[N+1];
+		Double point[][]=new Double[N+1][2],ans=0.0;ArrayList<Node>list=new ArrayList<>();
+		for(int i=1;i<N+1;i++){point[i][0]=s.nextDouble();point[i][1]=s.nextDouble();arr[i]=i;}
+		for(int i=1;i<N;i++){for(int j=i+1;j<N+1;j++)list.add(new Node(i,j,Math.sqrt(Math.pow(point[i][0]-point[j][0],2)+Math.pow(point[i][1]-point[j][1],2))));}
 		Collections.sort(list,(a,b)->Double.compare(a.d, b.d));
-		Double ans=0.0;
-		int pick=0;
-		for (int i=0;i<list.size();i++){
-			int px=findset(list.get(i).x);
-			int py=findset(list.get(i).y);
-			if (px!=py){
-				union(px,py);
-				ans+=list.get(i).d;
-				pick++;
-			}
+		for(int i=0;i<list.size();i++){
+			px=f(list.get(i).x);py=f(list.get(i).y);
+			if(px!=py){arr[py]=px;ans+=list.get(i).d;pick++;}
 			if(pick==N-1)break;
 		}
 		System.out.println(ans);
 	}
-
-	static void union(int x, int y) {
-		arr[y] = x;
-	}
-
-	static int findset(int x) {
-		if (x != arr[x])
-			arr[x] = findset(arr[x]);
-		return arr[x];
-	}
+	static int f(int x){if(x!=arr[x])arr[x]=f(arr[x]);return arr[x];}
 }
