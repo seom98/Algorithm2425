e79b4b8c868cb853a950ddc55c5a6ce3ac835377
seom98 <seo_m98@naver.com>
2024-12-09 22:31:15 +0900

    [Bronze IV] Title: 입실 관리, Time: 96 ms, Memory: 12940 KB, Score: 100 point -BaekjoonHub

---
+++ Java/백준/Bronze/5524. 입실 관리/입실 관리.java
@@ -0,0 +1,8 @@
+import java.util.*;
+interface Main{
+	static void main(String[]z){
+		Scanner s=new Scanner(System.in);
+		int N=s.nextInt();
+		while(N-->0){System.out.println(s.next().toLowerCase());}
+	}
+}
