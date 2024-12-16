c7b3b2287fec2b538aa2b8a0f2a79492192062f3
seom98 <seo_m98@naver.com>
2024-12-16 13:52:46 +0900

    [Silver I] Title: 카드 구매하기, Time: 164 ms, Memory: 17088 KB -BaekjoonHub

---
+++ Java/백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+interface Main{
+	static void main(String[]z){
+		Scanner s=new Scanner(System.in);
+		int N=s.nextInt(),dp[]=new int[N+1],a[]=new int[N+1];
+		for(int i=1;i<=N;i++)a[i]=s.nextInt();
+		for(int i=1;i<=N;i++)
+			for(int j=1;j<=i;j++)dp[i]=Math.max(dp[i],dp[i-j]+a[j]);
+		System.out.print(dp[N]);
+	}
+}
