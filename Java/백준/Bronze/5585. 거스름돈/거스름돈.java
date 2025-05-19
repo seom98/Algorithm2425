6716d2225b09c0397b3ccbc6d2aa529ac1606d07
seom98 <seo_m98@naver.com>
2025-05-19 21:43:27 +0900

    [Bronze II] Title: 거스름돈, Time: 164 ms, Memory: 17712 KB -BaekjoonHub

--- Java/백준/Bronze/5585. 거스름돈/거스름돈.java
+++ Java/백준/Bronze/5585. 거스름돈/거스름돈.java
@@ -1,10 +1,1 @@
-import java.util.*;
-class Main{
-	public static void main(String[]z){
-		Scanner s=new Scanner(System.in);
-		int C=1000-s.nextInt(),S=0;
-		int[]coin={500, 100, 50, 10, 5, 1};
-		for(int c:coin){S+=C/c;C%=c;}
-		System.out.print(S);
-	}
-}
+interface Main{static void main(String[]z){var s=new java.util.Scanner(System.in);int C=1000-s.nextInt(),S=0,c[]={500,100,50,10,5,1};for(int i:c){S+=C/i;C%=i;}System.out.print(S);}}
