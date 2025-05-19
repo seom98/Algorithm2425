677549af3b1b7eab3ac600c3b382c764a93f7afb
seom98 <seo_m98@naver.com>
2025-05-19 20:54:26 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 1084 ms, Memory: 564808 KB -BaekjoonHub

---
+++ Java/백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main{
+	public static void main(String[]z){
+		Scanner s=new Scanner(System.in);
+		String str=s.next();
+		int N=str.length(),c=0;
+		HashSet<String>set=new HashSet<>();
+		for(int i=1;i<=N;i++)for(int j=0;j<=N-i;j++)set.add(str.substring(j,j+i));
+		System.out.print(set.size());
+	}
+}
