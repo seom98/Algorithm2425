73e9e8bf49e8aca3ea890504fcdfbba8ac48dfb7
seom98 <seo_m98@naver.com>
2025-06-12 00:25:53 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 716 ms, Memory: 117220 KB -BaekjoonHub

--- Java/백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
+++ Java/백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -2,15 +2,14 @@ import java.util.*;
 interface Main{
 	static void main(String[]z){
 		Scanner s=new Scanner(System.in);
-		int N=s.nextInt()+1,M=s.nextInt()+1,a[][]=new int[N][M],Q;
-		for(int i=1;i<N;i++)for(int j=1;j<M;j++)a[i][j]=s.nextInt()+a[i][j-1];
-		Q=s.nextInt();
-		StringBuilder sb=new StringBuilder();
-		while(Q-->0){
-			int i=s.nextInt(),j=s.nextInt(),x=s.nextInt(),y=s.nextInt(),S=0;
-			for(int k=i;k<=x;k++)S+=a[k][y]-a[k][j-1];
-			sb.append(S+"\n");
+		int N=s.nextInt()+1,M=s.nextInt()+1,a[][]=new int[N][M],i,j;
+		for(i=1;i<N;i++)for(j=1;j<M;j++)a[i][j]=s.nextInt()+a[i][j-1];
+		N=s.nextInt();StringBuilder b=new StringBuilder();
+		while(N-->0){
+			int x[]=new int[4],S=0;for(i=0;i<4;i++)x[i]=s.nextInt();
+			for(i=x[0];i<=x[2];i++)S+=a[i][x[3]]-a[i][x[1]-1];
+			b.append(S+"\n");
 		}
-		System.out.println(sb);
+		System.out.print(b);
 	}
 }
